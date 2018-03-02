40c0ebcff990e5c5f452ee8c6e8eb426d70d06a6
Malcolm Foster <malcolmfoster@Malcolms-MacBook-Air.local>
2018-03-02 15:48:26 -0700

    initial push

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TheItems/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/TheItems.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheItems
+  },
+  data() {
+    return {
+      msg: "Hello Galvanize"
+      }
+  }
+}
+</script>
+
+<style>
+</style>
